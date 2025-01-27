7270429370d7f8ea1b83766bdf4dd1022d706aa1
frasanlli <146319006+frasanlli@users.noreply.github.com>
2025-01-27 10:20:40 +0100

    v.0.0.6 adding new app to admin page

--- service_app/admin.py
+++ service_app/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Service
 
 # Register your models here.
+
+class Admin_service(admin.ModelAdmin):
+    readonly_fields = ("created", "updated")
+    list_display = ("title", "content", "image", "created", "updated")
+    search_fields = ("title", "content")
+
+admin.site.register(Service, Admin_service)
