bed91f717f5fa06f8d1096be265653f04d91ce3c
frasanlli <146319006+frasanlli@users.noreply.github.com>
2025-01-28 14:03:36 +0100

    v.0.2.0 Blog can be filtered by categories

--- blog_app/urls.py
+++ blog_app/urls.py
@@ -5,6 +5,7 @@ from blog_app import views
 
 urlpatterns = [
     path('', views.blog, name='blog'),
-
+    path('category/<int:category_id>/',
+         views.category_filter, name='category'),
 ]
 
--- blog_app/views.py
+++ blog_app/views.py
@@ -1,8 +1,20 @@
 from django.shortcuts import render
-from .models import Post
+from .models import Post, Category
 
 # Create your views here.
 def blog (request):
 
     posts = Post.objects.all()
-    return render(request, "blog_app/blog.html", {"posts": posts})
+    categories = Category.objects.all()
+
+    return render(request, "blog_app/blog.html", {"posts": posts, "categories": categories})
+
+
+def category_filter (request, category_id):
+
+    category = Category.objects.get(id = category_id)
+    posts = Post.objects.filter(categories = category)
+    categories = Category.objects.all()
+
+    return render(request, "blog_app/category.html",
+                  {"category": category, "posts": posts, "categories": categories})
